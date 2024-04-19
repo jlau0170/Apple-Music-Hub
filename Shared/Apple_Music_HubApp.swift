b165e08ecf6ea31516b6660e043a0d0d37b43054
Justin Lau <jlauandpiano@gmail.com>
2024-04-18 17:02:50 -0700

    Initial Commit

---
+++ Shared/Apple_Music_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Music_HubApp.swift
+//  Shared
+//
+//  Created by Justin Lau on 2024-04-18.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Music_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
